37f835340604a6977f8a1059635045c3e3573371
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2012-06-17 22:21:20 +0200

    Mode environment variable definition for udp transport

--- torrent.js
+++ torrent.js
@@ -1,11 +1,12 @@
+process.env.KADOH_TRANSPORT = 'udp';
+
 var kadoh = require('kadoh');
-var url = require('url');
-var BittorrentNode = require('./lib/bittorrent-node');
+    url = require('url');
+    bittorrent = require('./lib/bittorrent-node');
 
 var shaReg = /[A-Za-z0-9]{40}/
 
-process.env.KADOH_TRANSPORT = 'udp';
-var torrent = new BittorrentNode(null, {
+var node = new bittorrent(null, {
   bootstraps: ['67.215.242.139:6881', '67.215.242.138:6881'],
   reactor : {
     type: 'udp',
@@ -30,14 +31,17 @@ function parseMagnetLink(uri) {
 }
 
 console.log("Connecting...");
-torrent.connect(function() {
+node.connect(function() {
   console.log("Joining...");
-  torrent.join(function() {
+  node.join(function() {
     console.log('Try torrent.get("#info_hash")');
     require('repl').start('torrent> ').context.torrent = {
-      node: torrent,
+      node: node,
       get: function(hash) {
-        torrent.get(parseMagnetLink(hash), function(arr) { console.log(arr); })
+        node.get(parseMagnetLink(hash), function(arr) { console.log(arr); })
+      },
+      announce: function() {
+        
       }
     }
   });
